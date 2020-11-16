e3349073cee7c1a2a376d75e51b5017da337bcbc
Gabriel Aguiar <gabrielaguiar1994@hotmail.com>
2020-11-16 14:04:36 -0300

    Delete files and add routes.tsx

---
+++ src/Routes.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Routes = () => {
+    return (
+        <div>Rotas</div>
+
+    );
+}
+export default Routes;
---
+++ src/core/components/NavBar/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NavBar = () => {
+    <nav>
+        <div>NavBar</div>
+    </nav>
+}
+
+export default NavBar;
